a2409930df3553b192ba282bf40a567cae220bca
jinchen1036 <jinchen1036@users.noreply.github.com>
2019-11-21 18:54:18 -0500

    test models with result

--- ModelProcessor.py
+++ ModelProcessor.py
@@ -8,14 +8,14 @@ class ModelProcessor:
     def train_model(self, classifier, parameters,feature_columns, train_df, test_df, grid = False, train_target='rent',cv_split = 6):
         SearchCV = CV_Model(GridSearch=grid)
         SearchCV.train_model(classifier, parameters, train_df[feature_columns],train_df[train_target], cv_split = cv_split)
-        self.test_set = test_df
+        self.test_set = test_df.copy()
         self.test_set['predictions'] = SearchCV.pred_target(test_df[feature_columns])
 
         loss = mean_squared_error(self.test_set['predictions'], self.get_fake_rent(train_df[train_target], test_df.shape[0]))
         print("Test Loss: %.4f" % loss)
 
     def get_fake_rent(self, train_target, test_df_shape):
-        return np.ones(test_df_shape * train_target.median())
+        return np.ones(test_df_shape) * train_target.median()
 
     def generate_result_csv(self, file_name):
         self.test_set['predictions'].to_csv(file_name, header=True)
