397b02c5f00233e088f041c5a975dadbfe30d14d
jinchen1036 <jinchen1036@users.noreply.github.com>
2019-11-28 22:42:58 -0500

    finalize external income dataset

--- Helper/DataLoader.py
+++ Helper/DataLoader.py
@@ -14,24 +14,25 @@ def load_dataset(numeric=True, extra_dataset=False):
     '''
     train_df = pd.read_csv('https://grantmlong.com/data/SE_rents2018_train.csv', index_col=0)
     test_set1_df = pd.read_csv('https://grantmlong.com/data/SE_rents2018_test1.csv', index_col=0)
-    test_set2_df = pd.read_csv('https://grantmlong.com/data/SE_rents2018_test2.csv', index_col=0)
+    # test_set2_df = pd.read_csv('https://grantmlong.com/data/SE_rents2018_test2.csv', index_col=0)
     test_set3_df = pd.read_csv('https://grantmlong.com/data/SE_rents2018_test3.csv', index_col=0)
 
     train_df_index = train_df.index
     test_set1_index = test_set1_df.index
-    test_set2_index = test_set2_df.index
+    # test_set2_index = test_set2_df.index
     test_set3_index = test_set3_df.index
-    combine_df = pd.concat([train_df,test_set1_df,test_set2_df,test_set3_df])
+    combine_df = pd.concat([train_df,test_set1_df,test_set3_df])
+    combine_df['addr_zip'] = combine_df['addr_zip'].astype(np.int64)
 
     if numeric:
         combine_df = combine_df.select_dtypes(include=[np.number])
     if not extra_dataset:
         combine_df.drop(['bin', 'bbl', 'building_id'], axis=1, inplace=True)
     else:
-        # external dataset source need to be modify after our csv file generated
-        external_df= pd.read_csv('https://data.cityofnewyork.us/resource/myei-c3fa.csv')
-        combine_df = combine_df.merge(external_df, on='bbl', how='left')
-    return combine_df, train_df_index, test_set1_index, test_set2_index, test_set3_index
+        external_df= pd.read_csv('https://raw.githubusercontent.com/jinchen1036/DataScience_Project/master/ExternalDF/zipcode_income_withrentid.csv?token=AKASCZIDUHRZH2GD24ZKT4C55HFAU', index_col=0)
+        combine_df['income'] = external_df.income
+
+    return combine_df, train_df_index, test_set1_index, test_set3_index
 
 
 def clean_data(dataset):
@@ -41,10 +42,12 @@ def clean_data(dataset):
        size_sqft -> replace any value greater than 8000 or equal to 0 with median
        year_built -> replace any value equal to 0 with median
        min_to_subway -> replace any value greater than to 100 with median
+       median_income_11249 -> 83,407
        fill_NA -> with median
     '''
 
     # Get all column with NAN and fill with median value
+    dataset.income.fillna(83407,inplace = True)
     must_process_columns = ['bathrooms','size_sqft','year_built','min_to_subway']
     nan_columns = dataset.columns[dataset.isna().any()].tolist()
     nan_columns = list(set(nan_columns + must_process_columns))
@@ -52,7 +55,7 @@ def clean_data(dataset):
         if column == 'rent':  # not make any change to rent
             continue
         median_value = (dataset[column]).median()
-        dataset[column] = dataset[column].fillna(median_value)
+        dataset[column].fillna(median_value,inplace = True)
         if column == 'bathrooms':
             dataset.loc[(dataset['bathrooms'] == 0) | (dataset['bathrooms'] > 7), 'bathrooms'] = median_value
         elif column == 'size_sqft':
